9de8b8515ace059085d7c47bb8e46c2bda2f4bf0
Nicholas Ng <nicholasykng@gmail.com>
2019-12-05 03:27:47 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,8 @@
 class CreateActors < ActiveRecord::Migration[5.1]
   def change
     create_table :actors do |t|
-      t.text :first_name
-      t.text :last_name
+      t.string :first_name
+      t.string :last_name
     end
   end
 end
