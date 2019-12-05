d153cf6ef64dd99ddc1e9e62d01831bdce1c37fa
Nicholas Ng <nicholasykng@gmail.com>
2019-12-05 03:07:11 +0000

    Automatically backed up by Learn

--- db/migrate/004_add_actors.rb
+++ db/migrate/004_add_actors.rb
@@ -1,6 +1,6 @@
 class AddActors < ActiveRecord::Migration[5.1]
   def change
-    add_table :actors do |t|
+    create_table :actors do |t|
       t.text :first_name
       t.text :last_name
     end
