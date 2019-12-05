ab82ddfab75482293d9f9e0394968b0c5581104c
Nicholas Ng <nicholasykng@gmail.com>
2019-12-05 03:25:18 +0000

    Automatically backed up by Learn

--- db/migrate/005_add_characters.rb
+++
@@ -1,9 +0,0 @@
-class AddCharacters < ActiveRecord::Migration[5.1]
-  def change
-    create_table :characters do |t|
-      t.text :name
-      t.integer :actor_id
-      t.integer :show_id
-    end
-  end
-end
